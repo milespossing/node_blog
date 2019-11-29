1dbbc5f1cc646cdb1e87004049faa71ce46fc919
Miles Possing <milespossing@gmail.com>
2019-11-29 13:29:59 -0600

    Adding mongo string test

--- app.js
+++ app.js
@@ -39,4 +39,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+console.log("Hello World");
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Miles Blog' });
+  res.render('index', { title: 'Miles Blog', testData: process.env.mongostring || "no string"});
 });
 
 module.exports = router;
